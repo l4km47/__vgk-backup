d15f2a8608dda1d477685156e887ece193a521c8
pikachui <i@DESKTOP-d73b>
2025-04-16 06:50:09 +0530

    vgk 1.17.4.2

--- offsets.h
+++ offsets.h
@@ -7,10 +7,10 @@
 #define PTRDIFF_ inline std::ptrdiff_t
 namespace offsets
 {
-//vgk 1.17.3.4
-	PTRDIFF_ vgk_shadow = 0x819C8;     //0F B6 05 ? ? ? ? 4C 8B 05 ? ? ? ?	  > mov     r8, cs:qword_140083F08
-	PTRDIFF_ qword_2	 = 0x81B50;		//48 8B 3D ?? ?? ?? ?? 49 33 D4		  > mov     rdi, cs:qword_140084090
-	PTRDIFF_ byte_1	 = 0x81A90;		//0F B6 35 ?? ?? ?? ?? 41 BC E8 00 00 00 > movzx   esi, cs:byte_140083FD0
+//vgk 1.17.4.2
+	PTRDIFF_ vgk_shadow = 0x82AC8;     //0F B6 05 ? ? ? ? 4C 8B 05 ? ? ? ?	  > mov     r8, cs:qword_140082AC8
+	PTRDIFF_ qword_2	 = 0x82C50;		//48 8B 3D ?? ?? ?? ?? 49 33 D4		  > mov     rdi, cs:qword_140082C50
+	PTRDIFF_ byte_1	 = 0x82B90;		//0F B6 35 ?? ?? ?? ?? 41 BC E8 00 00 00 > movzx   esi, cs:byte_140082B90
 }
 
 #endif
